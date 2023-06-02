9ec696ccfea032082a64207726465a4ee8059f6d
Matthew <mattchu18@gmail.com>
2023-06-02 16:26:42 -0700

    added tasker model and seeders

---
+++ app/models/tasker.py
@@ -0,0 +1,34 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Tasker(db.Model):
+    __tablename__ = "taskers"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable = False)
+    last_name = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    city = db.Column(db.String(255), nullable = False)
+    phone_number = db.Column(db.Integer(), nullable = False, unique = True)
+    bio = db.Column(db.String(500), nullable = False)
+    profile_image = db.Column(db.String(500))
+    vehicles = db.Column(db.String(500), nullable = False)
+    tools = db.Column(db.String(500), nullable = False)
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'city': self.city,
+            'phone_number': self.phone_number,
+            'bio': self.bio,
+            'profile_image': self.profile_image,
+            'vehicles': self.vehicles,
+            'tools': self.tools
+        }
---
+++ app/seeds/taskers.py
@@ -0,0 +1,58 @@
+from app.models.tasker import db, Tasker, environment, SCHEMA
+from sqlalchemy.sql import text
+
+def seed_taskers():
+    kevinb = Tasker(
+        username='kevinb_tasker',
+        first_name = "Kevin",
+        last_name = "B",
+        email='kevinb_tasker@aa.io',
+        city='Demo City',
+        phone_number= 110_000_0000,
+        bio = "Hi I am Kevin and I raise pokemons. I also can make a good cup of keureg coffee. I know how to operate and maintain them machines to sustain coffee consumption.",
+        profile_image = "imageforkevin.com",
+        vehicles = "Mercedes Benz S Class",
+        tools = "Pokedex, keureg machine, coffee beans, pokeballs"
+        )
+    vanessag = Tasker(
+        username='vanessag_tasker',
+        first_name = "Vanessa",
+        last_name = "G",
+        email='vanessag_tasker@aa.io',
+        city='Demo City',
+        phone_number= 120_000_0000,
+        bio = "Hi I am Vanessa and I am the hippesta and coolioest dating expert. I am also a budding software engineer that plans to create my own dating app and host my own dating tv show. I have successfully matched over 500,000 people. I am here to help.",
+        profile_image = "imageforvanessa.com",
+        vehicles = "Smart Car",
+        tools = "Keyboard, phone, therapist certification, influencer"
+        )
+    tonyh = Tasker(
+        username='tonyh_tasker',
+        first_name = "Tony",
+        last_name = "H",
+        email='tonyh_tasker@aa.io',
+        city='Demo City',
+        phone_number= 130_000_0000,
+        bio = "Hi I am Tony and I like spaghetti! When I was a young lad I fell on a wrench and that wrench was fused to my hand. Thus, I am able to fix anything that I encounter because I have a wrench handy at all times. My spaghetti always tastes like metal though.",
+        profile_image = "imagefortony.com",
+        vehicles = "Moped, Pickup Truck",
+        tools = "hammer, wrench, ladder, spatula, drill"
+        )
+    matthewa = Tasker(
+        username='tonyh_tasker',
+        first_name = "Matthew",
+        last_name = "A",
+        email='matthewa_tasker@aa.io',
+        city='Demo City',
+        phone_number= 140_000_0000,
+        bio = "Hi I am a dance instructer. I help people learn how to ballroom dance, hip-hop, salsa, and pole dance. I have over 1 year of experience and currently 2 students, Tony and Kevin. I am also a cheerleader on the side.",
+        profile_image = "imageformatthew.com",
+        vehicles = "Bicycle, walking",
+        tools = "pole, boombox, cane, towel, pom poms"
+        )
+
+    db.session.add(kevinb)
+    db.session.add(vanessag)
+    db.session.add(tonyh)
+    db.session.add(matthewa)
+    db.session.commit()
