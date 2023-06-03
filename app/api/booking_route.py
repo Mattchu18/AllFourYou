0ba3d9ca066521ab27ae3da3fdd009f5f55a347a
Tony <iamt.he@yahoo.com>
2023-06-03 13:10:21 -0700

    working on first route'

---
+++ app/api/booking_route.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, redirect
+from flask_login import login_required, current_user
+
+from ..models.booking import Booking
+from ..models.db import db
+# from ..forms.post_form import PostForm
+from datetime import date
+from random import randint
+
+booking_routes = Blueprint("bookings", __name__,url_prefix='')
+
+print(__name__, "Inside bookings blueprint")
+
+@booking_routes.route("/all")
+@login_required
+def get_all_bookings():
+    """
+    route to fetch and display all bookings for logged in user
+    """
+    all_bookings = Booking.query.all()
+    print(all_bookings)
+    return "ALL bookings page"
+
+# @booking_routes.route("")
+# def 
+# pass
