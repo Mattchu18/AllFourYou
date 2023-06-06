f4bae5cd408bb73c0e7f9bbb86bbdf6083da8830
Matthew <mattchu18@gmail.com>
2023-06-05 17:05:00 -0700

    redid our migrations folder

--- migrations/versions/3e93b69957a0.py
+++ migrations/versions/3e93b69957a0.py
@@ -8,7 +8,9 @@ Create Date: 2023-06-05 16:14:19.729562
 from alembic import op
 import sqlalchemy as sa
 
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 # revision identifiers, used by Alembic.
 revision = '3e93b69957a0'
 down_revision = None
@@ -81,6 +83,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
