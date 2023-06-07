98c910f216d9e177a86c4c44dd33dbacf9bae119
Vanessa Gonzalez <vxg026@gmail.com>
2023-06-06 21:46:55 -0700

    search bar done need event listener

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -5,16 +5,29 @@ const SearchBar = () =>{
 const [searchInput, setSearchInput] = useState("")
 const [searchResults, setSearchResults] = useState([])
 
+
 const handleChange = (e) => {
     e.preventDefault();
     setSearchInput(e.target.value)
 }
+
+const enter =(e)=>{
+    if (e.key==='Enter'){
+        e.preventDefault()
+        handleSearch()
+    }
+}
+//need to add event listener to submit when pressing enter.
+
+
 const handleSearch = ()=>{
-    fetch(`/search?query=${searchInput}`)
+    fetch(`/api/search/?query=${searchInput}`)
     .then((response)=> response.json())
     .then((data)=> setSearchResults(data.results))
     .catch((error)=>console.error(error))
 }
+
+
 return (
     <div>
         <input
@@ -24,13 +37,30 @@ return (
         value = {searchInput}
         />
     <button onClick={handleSearch}>Search</button>
-
-    {searchResults.length>0 ? (
+{console.log("search==========>", searchResults)}
+    {searchResults?.length>0 ? (
         searchResults.map((result)=>(
-            <div key={result.id}>{result.name}</div>
+            <>
+            {console.log(result, "jfdl;ajsfkldsfvhasvsa100-----------")}
+            <div key={result?.id}>{result?.bio}</div>
+            <div>{result?.city}</div>
+            <div>contact tasker: email: {result?.email}, phone number:{result?.phone}</div>
+            <div>{result?.profile_image}</div>
+            {result.tasks.map((task)=>(
+                <>
+
+                <div>{task?.category}</div>
+                <div>{task?.description}</div>
+                </>
+            ))}
+    
+            <br></br>
+
+            </>
+
         ))
     ): (
-        <div> No Results found </div>
+        <div> Sorry, no Results found :( </div>
     )}
     </div>
 )
