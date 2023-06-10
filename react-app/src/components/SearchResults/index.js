6f50c571d3e00ee93e8b4d54c4132a55927344c2
Matthew <mattchu18@gmail.com>
2023-06-09 19:47:47 -0700

    added styling for searchResults

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -1,59 +1,59 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import "./index.css"
 import { Fragment } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { useLocation } from 'react-router-dom';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import SearchBar from '../SearchBar';
 import { thunkAllTaskers } from '../../store/taskers';
 
-const SearchResults = () =>{
-const history=useHistory()
-const dispatch = useDispatch()
+const SearchResults = () => {
+  const history = useHistory()
+  const dispatch = useDispatch()
 
-const [searchResults, setSearchResults] = useState([])
-const allTaskersObj = useSelector(state => state.tasker.allTaskers)
-const allTaskers = Object.values(allTaskersObj)
-console.log(allTaskers[0])
-const location = useLocation();
-const searchInput = new URLSearchParams(location.search).get("query") || "";
+  const [searchResults, setSearchResults] = useState([])
+  const allTaskersObj = useSelector(state => state.tasker.allTaskers)
+  const allTaskers = Object.values(allTaskersObj)
+  console.log(allTaskers[0])
+  const location = useLocation();
+  const searchInput = new URLSearchParams(location.search).get("query") || "";
 
 
-useEffect(() => {
+  useEffect(() => {
     handleSearch();
   }, [searchInput]);
-// const handleChange = (e) => {
-//     e.preventDefault();
-    // setSearchInput(e.target.value)
-// }
+  // const handleChange = (e) => {
+  //     e.preventDefault();
+  // setSearchInput(e.target.value)
+  // }
 
 
-const enter =(e)=>{
-    if (e.key==='Enter'){
-        e.preventDefault()
-        handleSearch()
+  const enter = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      handleSearch()
     }
-}
-//need to add event listener to submit when pressing enter.
+  }
+  //need to add event listener to submit when pressing enter.
 
 
-const handleSearch = ()=>{
+  const handleSearch = () => {
     fetch(`/api/search/?query=${searchInput}`)
-    .then((response)=> response.json()
-    )
-    .then((data)=> setSearchResults(data.results))
-    .catch((error)=>console.error(error))
-}
+      .then((response) => response.json()
+      )
+      .then((data) => setSearchResults(data.results))
+      .catch((error) => console.error(error))
+  }
 
-const highlightText = (text) => {
+  const highlightText = (text) => {
     const lowerCaseText = text?.toLowerCase();
     const lowerCaseSearchInput = searchInput.toLowerCase();
     const parts = [];
     let startIndex = 0;
 
-    if(!searchInput){
-        return text;
+    if (!searchInput) {
+      return text;
     }
 
     while (startIndex < text?.length) {
@@ -74,10 +74,10 @@ const highlightText = (text) => {
     return parts;
   };
 
-return (
-  <section className="root">
-    <div>
-        <SearchBar searchInput={searchInput} handleSearch={handleSearch}/>
+  return (
+    <section className="root">
+      <div>
+        <SearchBar searchInput={searchInput} handleSearch={handleSearch} />
         {/* <input
         type = "search"
         placeholder="Search task"
@@ -85,89 +85,88 @@ return (
         value = {searchInput}
         onKeyDown={enter}
         /> */}
-    {/* <button onClick={handleSearch}>Search</button> */}
-    <div class="all-results">
+        {/* <button onClick={handleSearch}>Search</button> */}
+        <div class="all-results">
 
-    {searchResults?.length>0 ? (
-        searchResults.map((result)=>(
+          {searchResults?.length > 0 ? (
+            searchResults.map((result) => (
 
-            <>
-             <Link class="link-to-taskers" to={`/taskers/${result.id}`}>
-          <div className="test">
+              <>
+                <Link class="link-to-taskers" to={`/taskers/${result.id}`}>
+                  <div className="test">
 
-              <div>
+                    <div>
 
-              </div>
-            <div className="result-card">
-              <div class="profile-pic">
-                <div>
-                <img class="imag" src={result.url}/>
-              </div>
-              <div>
+                    </div>
+                    <div className="result-card">
+                      <div class="profile-pic">
+                        <div>
+                          <img class="imag" src={result.url} />
+                        </div>
 
-                <button className="contact-button">Contact tasker!</button>
-              </div>
-
-              </div>
-
-</div>
-
-          <div className="info">
-            <div className="name-price">
-              <div>
-                <h2>{highlightText(result?.firstName)}, {highlightText(result?.lastName[0])}</h2>
-              </div>
-              <div className="price">{highlightText(result?.price)}</div>
+                          <button className="contact-button">Contact tasker!</button>
 
 
-            </div>
+                      </div>
 
-          <div className="tasker-details">
-            <div><i class="fas fa-truck"></i>{highlightText(result?.vehicles)}</div>
+                    </div>
 
-            <div><i className="fas fa-toolbox"></i>{highlightText(result?.tools)}</div>
-          </div>
-
-            <div className="tasker-bio-card">
-              <div className='text-to-help'>
-                 How I can help:
-              </div>
+                    <div className="info">
+                      <div className="name-price">
+                        <div>
+                          <h2>{highlightText(result?.firstName)}, {highlightText(result?.lastName[0])}</h2>
+                        </div>
+                        <div className="price">{highlightText(result?.price)}</div>
 
 
-              <div className="tasker-bio" key={result?.id}>{highlightText(result?.bio)}</div>
-            <div>{highlightText(result?.city)}</div>
-            <div>contact tasker: email: {highlightText(result?.email)}, phone number:{highlightText(result?.phone)}</div>
-            {result.tasks.map((task)=>(
-                <>
+                      </div>
 
-                <div>{highlightText(task?.category)}</div>
-                <div>{highlightText(task?.description)}</div>
+                      <div className="tasker-details">
+                        <div><i class="fas fa-truck"></i>{highlightText(result?.vehicles)}</div>
 
-                </>
-            ))}
-            </div>
-            </div>
-</div>
-   </Link>
-            <br></br>
+                        <div><i className="fas fa-toolbox"></i>{highlightText(result?.tools)}</div>
+                      </div>
 
-            </>
+                      <div className="tasker-bio-card">
+                        <div className='text-to-help'>
+                          How I can help:
+                        </div>
 
 
+                        <div className="tasker-bio" key={result?.id}>{highlightText(result?.bio)}</div>
+                        <div>{highlightText(result?.city)}</div>
+                        <div>contact tasker: email: {highlightText(result?.email)}, phone number:{highlightText(result?.phone)}</div>
+                        {result.tasks.map((task) => (
+                          <>
 
-))
-):
-(
-    <div> Sorry, no Results found :( </div>
-    )
-    }
+                            <div>{highlightText(task?.category)}</div>
+                            <div>{highlightText(task?.description)}</div>
+
+                          </>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+                <br></br>
+
+              </>
 
 
-</div>
 
-    </div>
+            ))
+          ) :
+            (
+              <div> Sorry, no Results found :( </div>
+            )
+          }
+
+
+        </div>
+
+      </div>
     </section>
-)
+  )
 
 }
 
