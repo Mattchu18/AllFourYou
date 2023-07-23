964df396b4275659b4817978062b44585bb48967
Matthew <mattchu18@gmail.com>
2023-07-23 16:24:54 -0700

    started styling chat with other users

--- react-app/src/components/UserProfiles/UserProfile.js
+++ react-app/src/components/UserProfiles/UserProfile.js
@@ -1,4 +1,4 @@
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
 import { thunkAllUsers } from '../../store/session';
 import { useEffect } from "react";
@@ -7,42 +7,38 @@ import { thunkAllMessages, thunkUserMessages } from '../../store/messages';
 import Users from './Users';
 
 const UserProfile = () => {
-// const allUsers = useSelector(state=> state)
-const dispatch = useDispatch()
-const {userId} = useParams()
+    const dispatch = useDispatch()
+    const { userId } = useParams()
+
+
+    useEffect(() => {
+        dispatch(thunkAllUsers())
+        dispatch(thunkUserMessages())
+    }, [dispatch])
+    const users = useSelector(state => state.session.allUsers)
 
 
 
-useEffect(()=>{
-    dispatch(thunkAllUsers())
-    dispatch(thunkUserMessages())
-}, [dispatch])
-const users = useSelector(state=> state.session.allUsers)
+    return (
+        <div className='task-panda-community-center'>
+            <div className="task-panda-p">
+                <h2 className="task-panda-community">Task Panda Community!</h2>
+                <p>Meet other TaskPanda users and Chat with them! <br/>Just click on <i className="far fa-comments fa-lg"></i> to connect with them!</p>
 
-// const createUserMsg = (user) => {
+            {/* User component */}
 
-// }
+            <div className="users-container">
+                <h3 className="task-panda-top">Task Panda Users</h3>
+                {users && Object.values(users).map(user => {
+                    return (
+                        <Users key={user.id} user={user} />
 
-    return(
-        <>
-        <h2 className="task-panda-community">Task Panda Community!</h2>
-        <div className="task-panda-p">
-                    <p>Meet other TaskPanda users and Chat with them! Just click on the Chat Icon</p><i className="far fa-comments"></i><p>to connect with them!</p>
+                    )
+                })}
+            </div>
+            </div>
 
         </div>
-        {/* User component */}
-
-        <div className="users-container">
-        <h3 className="task-panda-top">Task Panda Users</h3>
-                    {users && Object.values(users).map(user=>{
-            return (
-                <Users key={user.id} user={user} />
-
-            )
-        })}
-        </div>
-
-        </>
     )
 }
 export default UserProfile;
